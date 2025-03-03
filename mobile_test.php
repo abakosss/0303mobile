9c7ff76b269e5a6b4d6e44c2dfafd4343ad2039e
Bakos Bence <bakosbence1@icloud.com>
2025-03-03 13:37:56 +0100

    :)

--- mobile_test.php
+++ mobile_test.php
@@ -1,27 +1,32 @@
 <?php
 
-require_once ("mobile.php")
+require_once("Mobile.php");
 
+//Tesztelés funkció
 function testGetAllMobile()
-{
-    $mobileOBJ = new Mobile();
+{  
+    //Létrehozzuk az objektumot
+    $mobileObj = new Mobile();
+    //Hívjuk a getAllMobile metódust
+    $mobiles = $mobileObj->getAllMobile();
 
-    $mobiles = $mobileOBJ->getAllMobile();
-
-
-    if(empty($mobiles))
+    //Ellenőrizzük, hogy a lekérdezés nem üres
+    if (empty($mobiles))
     {
-        echo "nincs adat a mobileszközről.\n";
+        echo "Nincs adat a mobileszközről.\n";
     }
     else
     {
-        echo "Mobilok Listája:\n";
+        echo "Mobilok listája:\n";
+   
+        foreach($mobiles as $mobile)
+        {
+            echo "ID: " . $mobile['m_id'] . " | Név: " . $mobile["m_desc"] . "\n";
+        }
     }
-    foreach($mobiles as $mobile)
-    {
-        echo"ID:".$mobile['m_id']."| Név: " . $mobile["m_desc"] . "\n";
-    }
-
-    testGetAllMobile();
 }
+
+//Teszt fttatása
+testGetAllMobile();
+
 ?>
